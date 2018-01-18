1f5c3841135cce25ce5aa96355c1e805cc688f1c
Kevin Yank <thatguy@kevinyank.com>
2018-01-18 17:22:34 +1100

    Detect shade/tint

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 module.exports = function nearest(brandColors, color) {
-  const colorNumber = color.rgbNumber();
+  const hue = color.hue();
 
   if (Object.keys(brandColors).length === 0) return null;
 
   const nearestKey = Object.keys(brandColors).reduce((lastFound, key) => {
-    if (brandColors[key].rgbNumber() === colorNumber) return key;
+    if (brandColors[key].hue() === hue) return key;
     return lastFound;
   }, Object.keys(brandColors)[0]);
 
-  return { color: nearestKey };
+  const result = { color: nearestKey };
+
+  const d = delta(brandColors[nearestKey], color);
+  if (d < 0) {
+    result.shade = shade(brandColors[nearestKey], color);
+  }
+  if (d > 0) {
+    result.tint = tint(brandColors[nearestKey], color);
+  }
+
+  return result;
 };
+
+function delta(base, target) {
+  return target.lightness() - base.lightness();
+}
+
+function shade(base, target) {
+  const baseShade = base.lightness();
+  const targetShade = target.lightness();
+  return parseFloat((1 - targetShade / baseShade).toFixed(2));
+}
+
+function tint(base, target) {
+  const baseShade = base.lightness();
+  const targetShade = target.lightness();
+  return parseFloat(((targetShade - baseShade) / (100 - baseShade)).toFixed(2));
+}
--- test/test.js
+++ test/test.js
@@ -3,32 +3,12 @@ const nearest = require('../index');
 const Color = require('color');
 
 describe('nearest', () => {
-  describe('no color', () => {
-    const brandColors = {};
-
-    it('should return null', () => {
-      expect(nearest(brandColors, Color('#000000'))).to.be.null;
-    });
-  });
-
-  describe('single color', () => {
-    const brandColors = {
-      coral: Color('#f04c5d'),
-    };
-
-    it('should return the brand color', () => {
-      expect(nearest(brandColors, Color('#000000'))).to.eql({
-        color: 'coral',
-      });
-    });
-  });
+  const brandColors = {
+    coral: Color('#f04c5d'),
+    seedling: Color('#45ad8f'),
+  };
 
   describe('exact match', () => {
-    const brandColors = {
-      coral: Color('#f04c5d'),
-      seedling: Color('#45ad8f'),
-    };
-
     describe('for coral', () => {
       it('should return the matching color', () => {
         expect(nearest(brandColors, Color('#f04c5d'))).to.eql({
@@ -43,6 +23,45 @@ describe('nearest', () => {
           color: 'seedling',
         });
       });
+    });
+  });
+
+  describe('exact shade (added black)', () => {
+    it('it should return the matching shade of coral', () => {
+      expect(
+        nearest(brandColors, Color('#f04c5d').mix(Color('black'), 0.5))
+      ).to.eql({
+        color: 'coral',
+        shade: 0.5,
+      });
+    });
+
+    it('it should return the matching shade of seedling', () => {
+      expect(
+        nearest(brandColors, Color('#45ad8f').mix(Color('black'), 0.5))
+      ).to.eql({
+        color: 'seedling',
+        shade: 0.5,
+      });
+    });
+  });
+
+  describe('exact tint (added white)', () => {
+    it('it should return the matching tint', () => {
+      expect(
+        nearest(brandColors, Color('#f04c5d').mix(Color('white'), 0.5))
+      ).to.eql({
+        color: 'coral',
+        tint: 0.5,
+      });
+    });
+  });
+
+  describe('no brand colors', () => {
+    const brandColors = {};
+
+    it('should return null', () => {
+      expect(nearest(brandColors, Color('#000000'))).to.be.null;
     });
   });
 });
