2da0e15167e9665b8fc8e05faf7f9b5f68d14d34
Kevin Yank <thatguy@kevinyank.com>
2018-01-19 11:01:27 +1100

    Add CLI

---
+++ bin/cli.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+const findConfig = require('find-config');
+const nearest = require('..');
+const Color = require('color');
+const pkgName = require('../package.json').name;
+
+const config = findConfig.require('nearest-color.json');
+if (!config) {
+  console.log(pkgName + ' configuration not found');
+  process.exit(-1);
+}
+
+const brandColors = Object.keys(config).reduce(
+  (colors, key) => Object.assign(colors, { [key]: Color(config[key]) }),
+  {}
+);
+
+if (process.argv.length <= 2) {
+  console.log('Usage: ' + pkgName + ' <color>');
+  process.exit(-1);
+}
+
+console.log(nearest(brandColors, Color(process.argv[2])));
